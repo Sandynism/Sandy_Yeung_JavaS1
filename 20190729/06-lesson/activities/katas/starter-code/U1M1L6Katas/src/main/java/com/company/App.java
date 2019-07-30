8dfc407e3401d14623c9be47012fdbaa9413b6d1
sandynism <31261960+Sandynism@users.noreply.github.com>
2019-07-30 10:56:58 -0400

    challenges

--- 20190729/06-lesson/activities/katas/starter-code/U1M1L6Katas/src/main/java/com/company/App.java
+++ 20190729/06-lesson/activities/katas/starter-code/U1M1L6Katas/src/main/java/com/company/App.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Arrays;
 
 public class App {
 
@@ -84,7 +85,7 @@ public class App {
 
     //Every 3rd: Create a method called everyThird which takes in an array of ints and returns an array which consists of every third element. Your code should work for an array of any size. If there are fewer than three elements in the array, return null.
     public static int[] everyThird(int[] array) {
-        if(array.length < 3) {
+        if (array.length < 3) {
             return null;
         }
 
@@ -92,8 +93,8 @@ public class App {
         int[] newArray = new int[newArrayLength];
 
         int counter = 0;
-        for(int i = 0; i < array.length; i++) {
-            if ((i+1) % 3 == 0) {
+        for (int i = 0; i < array.length; i++) {
+            if ((i + 1) % 3 == 0) {
                 newArray[counter] = array[i];
                 counter++;
             }
@@ -128,22 +129,116 @@ public class App {
         return newArray;
     }
 
+    //Split at 5: Create a method called splitAtFive which takes in an array of ints. This method should split the array into two new arrays, one holding the values less than 5 and the other holding the values equal to or greater than 5. Return a two-dimensional array, with the small-value array first. Your code should work for an array of any size.
+    //int[][] test = new int[#_of_rows][#_of_columns]
+    //test.length = #_of_rows
+
+    public static int[][] splitAtFive(int[] numbers) {
+            int numLessThanFive = 0;
+            int numMoreThanFive = 0;
+
+            for(int num : numbers) {
+                if ( num < 5 ) {
+                    numLessThanFive++;
+                } else {
+                    numMoreThanFive++;
+                }
+            }
+
+            int[] lessThan = new int[numLessThanFive];
+            int[] moreThan = new int[numMoreThanFive];
+
+
+            for(int num : numbers) {
+
+                // subtracting numLessThanFive from length then decrementing numLessThanFive
+                // allows us to go from 0 to length - 1 in order without additional variables
+                // same with numMoreThanFive
+                if ( num < 5 ) {
+                    lessThan[lessThan.length - numLessThanFive] = num;
+                    numLessThanFive--;
+                } else {
+                    moreThan[moreThan.length - numMoreThanFive] = num;
+                    numMoreThanFive--;
+                }
+            }
+
+            return new int[][] { lessThan, moreThan };
+        }
+
+//    public static int[][] splitAtFive(int[] array) {
+//
+//        int counter = 0;
+//        int[] smallArray;
+//        int[] largeArray;
+//
+//        for(int i = 0; i < array.length; i++) {
+//            if(array[i] < 5) {
+//                counter++;
+//            }
+//        }
+//        smallArray = new int[counter];
+////        int[] firstArray = Arrays.copyOf(smallArray, counter);
+//        counter = 0;
+//
+//        for(int i = 0; i < array.length; i ++) {
+//            if(array[i] >= 5) {
+//                counter++;
+//            }
+//        }
+//        largeArray = new int[counter];
+////        int[] secondArray = Arrays.copyOf(largeArray, counter);
+//
+//        int[][] newArray = new int[smallArray.length][largeArray.length];
+//        System.out.println(Arrays.toString(newArray));
+//        return newArray;
+//    }
+
+    //Evens and Odds: Create a method called evensAndOdds which takes in an array of Strings. Create two new arrays, one holding the values of the even indices and the other the values of the odd. Return a two-dimensional array, with the even-indices array first. Your code should work for an array of any size.
+    public static String[][] evensAndOdds(String[] strings) {
+
+        String[] odds = new String[ strings.length/2 ];
+
+        String[] evens = null;
+
+        if (strings.length % 2 == 0) {
+            evens = new String[ strings.length/2];
+        } else {
+            evens = new String[ strings.length/2 + 1];
+        }
+
+        for(int i = 0; i < strings.length; i++) {
+            int currIndex = i/2;
+            if( i % 2 == 0 ) {
+                evens[currIndex] = strings[i];
+            } else {
+                odds[currIndex] = strings[i];
+            }
+        }
+        return new String[][] { evens, odds };
+    }
+
+
 
     public static void main(String[] args) {
 
-        int[] testArray = {1, 2, 3, 4, 5,6};
+        int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         String[] wordsTestArray = {"bird", "cat", "dog", "fish"};
 
-       System.out.println(total(testArray));
-       System.out.println(totalOdd(testArray));
-       System.out.println(totalEven(testArray));
-       System.out.println(secondLargestNumber(testArray));
-       System.out.println(swapFirstAndLast(wordsTestArray));
-       System.out.println(reverse(testArray));
-       System.out.println(concatenateString(wordsTestArray));
-       System.out.println(everyThird(testArray));
-       System.out.println(lessThanFive(testArray));
+        System.out.println(total(testArray));
+        System.out.println(totalOdd(testArray));
+        System.out.println(totalEven(testArray));
+        System.out.println(secondLargestNumber(testArray));
+        System.out.println(swapFirstAndLast(wordsTestArray));
+        System.out.println(reverse(testArray));
+        System.out.println(concatenateString(wordsTestArray));
+        System.out.println(everyThird(testArray));
+        System.out.println(lessThanFive(testArray));
+        System.out.println(splitAtFive(testArray));
+        System.out.println(evensAndOdds(wordsTestArray));
     }
 }
+
+
 
 
