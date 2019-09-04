382a3842fdec85aba1b155b5c84f35ab183b0165
sandynism <31261960+Sandynism@users.noreply.github.com>
2019-09-04 11:41:26 -0400

    adding console, game, tshirt, invoice tests and impls

--- SandyYeungU1Capstone/src/main/java/com/company/SandyYeungU1Capstone/dao/TshirtDaoJdbcTemplateImpl.java
+++ SandyYeungU1Capstone/src/main/java/com/company/SandyYeungU1Capstone/dao/TshirtDaoJdbcTemplateImpl.java
@@ -1,42 +1,107 @@
 package com.company.SandyYeungU1Capstone.dao;
 
 import com.company.SandyYeungU1Capstone.model.Tshirt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class TshirtDaoJdbcTemplateImpl implements TshirtDao {
+
+    private static final String SELECT_ALL_TSHIRTS_SQL =
+            "select * from t_shirt";
+
+    private static final String SELECT_TSHIRT_SQL =
+            "select * from t_shirt where t_shirt_id = ?";
+
+    private static final String INSERT_TSHIRT_SQL =
+            "insert into t_shirt (size, color, description, price, quantity) values (?,?,?,?,?)";
+
+    private static final String DELETE_TSHIRT_SQL =
+            "delete from t_shirt where t_shirt_id = ?";
+
+    private static final String UPDATE_TSHIRT_SQL =
+            "update t_shirt set size = ?, color = ?, description = ?, price = ?, quantity = ? where t_shirt_id = ?";
+
+    private static final String SELECT_TSHIRTS_BY_COLOR_SQL =
+            "select * from t_shirt where color = ?";
+
+    private static final String SELECT_TSHIRTS_BY_SIZE_SQL =
+            "select * from t_shirt where size = ?";
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public TshirtDaoJdbcTemplateImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private Tshirt mapRowToTshirt(ResultSet rs, int rowNum) throws SQLException {
+        Tshirt tshirt = new Tshirt();
+        tshirt.setTshirtId(rs.getInt("t_shirt_id"));
+        tshirt.setSize(rs.getString("size"));
+        tshirt.setColor(rs.getString("color"));
+        tshirt.setDescription(rs.getString("description"));
+        tshirt.setPrice(rs.getBigDecimal("price"));
+        tshirt.setQuantity(rs.getInt("quantity"));
+
+        return tshirt;
+    }
+
     @Override
     public List<Tshirt> getAllTshirts() {
-        return null;
+        return jdbcTemplate.query(SELECT_ALL_TSHIRTS_SQL, this::mapRowToTshirt);
     }
 
     @Override
     public Tshirt getTshirt(int tshirtId) {
-        return null;
+        try {
+            return jdbcTemplate.queryForObject(SELECT_TSHIRT_SQL, this::mapRowToTshirt, tshirtId);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     @Override
+    @Transactional
     public Tshirt addTshirt(Tshirt tshirt) {
-        return null;
+        jdbcTemplate.update(INSERT_TSHIRT_SQL, tshirt.getSize(), tshirt.getColor(), tshirt.getDescription(), tshirt.getPrice(), tshirt.getQuantity());
+        int id = jdbcTemplate.queryForObject("select last_insert_id()", Integer.class);
+        tshirt.setTshirtId(id);
+        return tshirt;
     }
 
     @Override
     public void deleteTshirt(int tshirtId) {
-
+        jdbcTemplate.update(DELETE_TSHIRT_SQL, tshirtId);
     }
 
     @Override
     public void updateTshirt(Tshirt tshirt) {
-
+        jdbcTemplate.update(UPDATE_TSHIRT_SQL, tshirt.getSize(), tshirt.getColor(), tshirt.getDescription(), tshirt.getPrice(), tshirt.getQuantity(), tshirt.getTshirtId());
     }
 
     @Override
     public List<Tshirt> getTshirtsByColor(String color) {
-        return null;
+        return jdbcTemplate.query(SELECT_TSHIRTS_BY_COLOR_SQL, this::mapRowToTshirt, color);
     }
 
     @Override
     public List<Tshirt> getTshirtsBySize(String size) {
-        return null;
+        return jdbcTemplate.query(SELECT_TSHIRTS_BY_SIZE_SQL, this::mapRowToTshirt, size);
     }
 }
+
+
+//    t_shirt_id int(11) not null auto_increment primary key,
+//    size varchar(20) not null,
+//    color varchar(20) not null,
+//    description varchar(255) not null,
+//    price decimal(5,2) not null,
+//    quantity int(11) not null
--- SandyYeungU1Capstone/src/test/java/com/company/SandyYeungU1Capstone/dao/GameDaoTest.java
+++ SandyYeungU1Capstone/src/test/java/com/company/SandyYeungU1Capstone/dao/GameDaoTest.java
@@ -1,45 +1,196 @@
 package com.company.SandyYeungU1Capstone.dao;
 
+import com.company.SandyYeungU1Capstone.model.Game;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class GameDaoTest {
+
+    @Autowired
+    GameDao gameDao;
 
     @Before
     public void setUp() throws Exception {
+        List<Game> gamesList = gameDao.getAllGames();
+        for(Game g : gamesList) {
+            gameDao.deleteGame(g.getGameId());
+        }
     }
 
     @Test
     public void getAllGames() {
+        Game game = new Game();
+        game.setTitle("Rayman Raving Rabbids");
+        game.setEsrbRating("E");
+        game.setDescription("The series focuses on wild rabbit-like creatures known as Rabbids, who like to cause havoc and mischief, speak gibberish and yell.");
+        game.setPrice(new BigDecimal("39.99"));
+        game.setStudio("Ubisoft Motion Pictures");
+        game.setQuantity(25);
+
+        gameDao.addGame(game);
+
+        game = new Game();
+        game.setTitle("Dance Dance Revolution");
+        game.setEsrbRating("E");
+        game.setDescription("A dance party where you use arrows to match the beats.");
+        game.setPrice(new BigDecimal("59.99"));
+        game.setStudio("Konami");
+        game.setQuantity(15);
+
+        gameDao.addGame(game);
+
+        List<Game> gamesList = gameDao.getAllGames();
+        assertEquals(gamesList.size(), 2);
     }
 
     @Test
-    public void getGame() {
+    public void addGetDeleteGame() {
+        Game game = new Game();
+        game.setTitle("Rayman Raving Rabbids");
+        game.setEsrbRating("E");
+        game.setDescription("The series focuses on wild rabbit-like creatures known as Rabbids, who like to cause havoc and mischief, speak gibberish and yell.");
+        game.setPrice(new BigDecimal("39.99"));
+        game.setStudio("Ubisoft Motion Pictures");
+        game.setQuantity(25);
+
+        gameDao.addGame(game);
+
+        Game game1 = gameDao.getGame(game.getGameId());
+        assertEquals(game, game1);
+
+        gameDao.deleteGame(game.getGameId());
+        game1 = gameDao.getGame(game.getGameId());
+        assertNull(game1);
     }
 
-    @Test
-    public void addGame() {
-    }
-
-    @Test
-    public void deleteGame() {
-    }
 
     @Test
     public void updateGame() {
+        Game game = new Game();
+        game.setTitle("Rayman Raving Rabbids");
+        game.setEsrbRating("E");
+        game.setDescription("The series focuses on wild rabbit-like creatures known as Rabbids, who like to cause havoc and mischief, speak gibberish and yell.");
+        game.setPrice(new BigDecimal("39.99"));
+        game.setStudio("Ubisoft Motion Pictures");
+        game.setQuantity(25);
+
+        gameDao.addGame(game);
+
+        game.setQuantity(30);
+
+        gameDao.updateGame(game);
+
+        Game game1 = gameDao.getGame(game.getGameId());
+        assertEquals(game, game1);
     }
 
     @Test
     public void getGamesByStudio() {
+        Game game = new Game();
+        game.setTitle("Rayman Raving Rabbids");
+        game.setEsrbRating("E");
+        game.setDescription("The series focuses on wild rabbit-like creatures known as Rabbids, who like to cause havoc and mischief, speak gibberish and yell.");
+        game.setPrice(new BigDecimal("39.99"));
+        game.setStudio("Ubisoft Motion Pictures");
+        game.setQuantity(25);
+
+        gameDao.addGame(game);
+
+        game = new Game();
+        game.setTitle("Dance Dance Revolution");
+        game.setEsrbRating("E");
+        game.setDescription("A dance party where you use arrows to match the beats.");
+        game.setPrice(new BigDecimal("59.99"));
+        game.setStudio("Konami");
+        game.setQuantity(15);
+
+        gameDao.addGame(game);
+
+        List<Game> gamesList = gameDao.getGamesByStudio("Ubisoft Motion Pictures");
+        assertEquals(gamesList.size(), 1);
+
+        gamesList = gameDao.getGamesByStudio("Konami");
+        assertEquals(gamesList.size(), 1);
+
+        gamesList = gameDao.getGamesByStudio("Studio");
+        assertEquals(gamesList.size(), 0);
     }
 
     @Test
     public void getGamesByEsrbRating() {
+        Game game = new Game();
+        game.setTitle("Rayman Raving Rabbids");
+        game.setEsrbRating("E");
+        game.setDescription("The series focuses on wild rabbit-like creatures known as Rabbids, who like to cause havoc and mischief, speak gibberish and yell.");
+        game.setPrice(new BigDecimal("39.99"));
+        game.setStudio("Ubisoft Motion Pictures");
+        game.setQuantity(25);
+
+        gameDao.addGame(game);
+
+        game = new Game();
+        game.setTitle("Dance Dance Revolution");
+        game.setEsrbRating("E");
+        game.setDescription("A dance party where you use arrows to match the beats.");
+        game.setPrice(new BigDecimal("59.99"));
+        game.setStudio("Konami");
+        game.setQuantity(15);
+
+        gameDao.addGame(game);
+
+        List<Game> gamesList = gameDao.getGamesByEsrbRating("E");
+        assertEquals(gamesList.size(), 2);
+
+        gamesList = gameDao.getGamesByEsrbRating("Esrb Rating");
+        assertEquals(gamesList.size(), 0);
     }
 
     @Test
     public void getGamesByTitle() {
+        Game game = new Game();
+        game.setTitle("Rayman Raving Rabbids");
+        game.setEsrbRating("E");
+        game.setDescription("The series focuses on wild rabbit-like creatures known as Rabbids, who like to cause havoc and mischief, speak gibberish and yell.");
+        game.setPrice(new BigDecimal("39.99"));
+        game.setStudio("Ubisoft Motion Pictures");
+        game.setQuantity(25);
+
+        gameDao.addGame(game);
+
+        game = new Game();
+        game.setTitle("Dance Dance Revolution");
+        game.setEsrbRating("E");
+        game.setDescription("A dance party where you use arrows to match the beats.");
+        game.setPrice(new BigDecimal("59.99"));
+        game.setStudio("Konami");
+        game.setQuantity(15);
+
+        gameDao.addGame(game);
+
+        List<Game> gamesList = gameDao.getGamesByTitle("Rayman Raving Rabbids");
+        assertEquals(gamesList.size(), 1);
+
+        gamesList = gameDao.getGamesByTitle("Title");
+        assertEquals(gamesList.size(), 0);
     }
 }
+
+
+//    game_id int(11) not null auto_increment primary key,
+//    title varchar(50) not null,
+//    esrb_rating varchar(50) not null,
+//    description varchar(255) not null,
+//    price decimal(5, 2) not null,
+//    studio varchar(50) not null,
+//    quantity int(11)
