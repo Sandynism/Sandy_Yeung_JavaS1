4df364111c3bf6b687716a0ec312cbda5634cdf8
sandynism <31261960+Sandynism@users.noreply.github.com>
2019-09-25 09:54:45 -0400

    adding spring security tutorials

---
+++ 20190925/03-module-security-reliability/01-lesson-spring-security/activities/in-class/secure-rsvp/src/main/java/com/trilogyed/securersvp/SecureRsvpApplication.java
@@ -0,0 +1,14 @@
+package com.trilogyed.securersvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureRsvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureRsvpApplication.class, args);
+	}
+
+}
+
